e7fea0e03ea01aac206d24b70c34c42dde3dd15f
Erika <erikadeo.lima@hotmail.com>
2024-07-16 20:52:30 -0300

    improve the navigation page

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+window.addEventListener("load", function() {
+    document.getElementById("about").scrollIntoView();
+  });
+
+  let aboutTop = document.getElementById('about').offsetTop;
+
+  window.addEventListener('scroll', function(event) {
+    let scrollPosition = window.scrollY;
+    if (scrollPosition < aboutTop) {
+      window.scrollTo(0, aboutTop);
+    }
+  });
+
 const menuButton = document.querySelector('.menu');
 const menuSandwich = document.querySelector('.menu-sandwich');
 
